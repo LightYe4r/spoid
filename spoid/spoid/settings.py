d7c8e5437288508642f552f88388adf0fe8c90d3
LightYe4r <24adrenalin@naver.com>
2024-07-17 14:09:25 +0900

    FIX: django secret key export .env

--- spoid/spoid/settings.py
+++ spoid/spoid/settings.py
@@ -39,7 +39,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&un%2$5t2_s^o%rns56o2uizzhl9-xxax%67flecy8=0i6efm'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
